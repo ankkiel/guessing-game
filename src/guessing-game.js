f58cab4d27d5b90136c1c302671aced6b25e4407
Anastasia Ermolova <anastasia_ermolova@bk.ru>
2022-08-10 08:55:04 +0300

    feat: implement binary search algorithm

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,27 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.low = 0;
+        this.high = 0;
+        this.mid = 0;
+    }
 
     setRange(min, max) {
-
+        this.low = min;
+        this.high = max;
     }
 
     guess() {
-
+        return (
+            this.mid = Math.ceil((this.low + this.high) / 2)
+        );
     }
 
     lower() {
-
+        this.setRange(this.low, this.mid);
     }
 
     greater() {
-
+        this.setRange(this.mid, this.high);
     }
 }
 
